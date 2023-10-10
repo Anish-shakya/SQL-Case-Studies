568a8fec09b180fbf95c30ecad8dfb06e394c370
Anish Shakya <shakyaanish69@gmail.com>
2023-10-10 17:45:40 +0545

    more solution

--- CS1_Danny's_Dinner/ProblemSolution.sql
+++ CS1_Danny's_Dinner/ProblemSolution.sql
@@ -13,17 +13,16 @@ GROUP BY customer_id
 ORDER BY 1
 
 -- What was the first item from the menu purchased by each customer?
-With CTE AS (
-	SELECT s.customer_id, s.order_date,m.product_name,
-	DENSE_RANK() OVER (PARTITION BY s.customer_id ORDER BY s.order_date) AS rank
-	FROM sales AS s
-	JOIN menu AS m
-	ON s.product_id = m.product_id
-	GROUP BY 1,2,3
-)
+WITH CTE AS (SELECT s.customer_id,m.product_name,s.order_date,
+DENSE_RANK() OVER(PARTITION BY s.customer_id order by s.order_date) AS ranks
+FROM sales AS s
+JOIN menu AS m
+ON s.product_id = m.product_id
+GROUP BY 1,2,3)
 SELECT customer_id,product_name
 FROM CTE
-WHERE rank = 1;
+WHERE ranks=1
+
 -- What is the most purchased item on the menu and how many times was it purchased by all customers?
 SELECT m.product_name,COUNT(*)
 FROM sales AS s
@@ -35,7 +34,36 @@ LIMIT 1
 
 
 -- Which item was the most popular for each customer?
+WITH CTE AS (
+SELECT customer_id,product_name,item_count,
+DENSE_RANK() OVER (PARTITION BY customer_id ORDER BY item_count DESC) AS ranks
+FROM(
+	SELECT s.customer_id,s.product_id,m.product_name,COUNT(*) AS item_count
+	FROM sales s
+	JOIN menu m
+	ON s.product_id= m.product_id
+	GROUP by s.product_id,s.customer_id,m.product_name
+	ORDER by s.customer_id,item_count DESC) AS popular_items)
+SELECT customer_id,product_name as popular_product
+FROM CTE
+WHERE ranks = 1
+
+
 --Which item was purchased first by the customer after they became a member?
+WITH CTE AS (
+	SELECT m.customer_id,m.join_date,s.product_id,s.order_date,
+	DENSE_RANK() OVER(PARTITION BY m.customer_id ORDER BY s.order_date) as ranks
+	FROM members m
+	JOIN sales s
+	ON m.customer_id  = s.customer_id 
+	WHERE s.order_date >= m.join_date -- filtering member and non member customer
+	GROUP BY 1,2,3,4
+)
+SELECT customer_id,m.product_name FROM CTE c
+JOIN menu m
+ON c.product_id = m.product_id
+WHERE ranks = 1
+
 -- Which item was purchased just before the customer became a member?
 -- What is the total items and amount spent for each member before they became a member?
 -- If each $1 spent equates to 10 points and sushi has a 2x points multiplier 
