d47d9b7d74ea8fd94b884a163afb1403c9649592
Anish Shakya <shakyaanish69@gmail.com>
2023-10-19 18:15:31 +0545

    solution

--- CS2_Pizza_Runner/ProblemSolution.sql
+++ CS2_Pizza_Runner/ProblemSolution.sql
@@ -10,8 +10,19 @@ GROUP BY runner_id
 ORDER BY runner_id;
 
 --4. How many of each type of pizza was delivered?
+SELECT p.pizza_name,COUNT(*) AS total_Delivered FROM customer_orders c
+JOIN runner_orders r ON c.order_id = r.order_id
+JOIN pizza_names p ON c.pizza_id = p.pizza_id
+WHERE r.pickup_time <> 'null'
+GROUP BY p.pizza_name;
 --5. How many Vegetarian and Meatlovers were ordered by each customer?
+SELECT c.customer_id,p.pizza_name,COUNT(*) AS num_ordered FROM customer_orders c
+JOIN pizza_names p ON c.pizza_id = p.pizza_id
+GROUP BY c.customer_id,p.pizza_name
+ORDER BY c.customer_id;
+
 --6. What was the maximum number of pizzas delivered in a single order?
+
 --7. For each customer, how many delivered pizzas had at least 1 change and how many had no changes?
 --8. How many pizzas were delivered that had both exclusions and extras?
 --9. What was the total volume of pizzas ordered for each hour of the day?
